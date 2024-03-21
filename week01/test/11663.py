2584542ea6ad8384b7612ff7e38e088cb53886dd
Yoosun-Chang <12201321@inha.edu>
2024-03-21 15:55:25 +0900

    test review

--- week01/test/11663.py
+++ week01/test/11663.py
@@ -1,24 +1,43 @@
-n,m = map(int,input().split())
-points = list(map(int,input().split()))
-lines = []
-for _ in range(m):
-    lines.append(list(map(int, input().split())))
+# bisect
+# import bisect
+
+# N, M = map(int, input().split())
+# points = list(map(int, input().split()))
+# lines = [list(map(int, input().split())) for _ in range(M)]
+
+# points.sort()
+
+# for line in lines:
+#     start, end = line
+#     start_index = bisect.bisect_left(points, start)
+#     end_index = bisect.bisect_right(points, end)
+#     count = end_index - start_index
+#     print(count)
+
+
+def binary_search(array, target):
+    # 이분 탐색을 통해 target 이상이 처음으로 나타나는 인덱스를 찾음
+    left, right = 0, len(array)
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+
+# 입력 받기
+N, M = map(int, input().split())
+points = list(map(int, input().split()))
+lines = [list(map(int, input().split())) for _ in range(M)]
+
+# 점들 정렬
 points.sort()
-cnt = [0]*n
 
+# 각 선분에 대해 이분 탐색을 통해 해당 선분 위에 있는 점의 개수를 구함
 for line in lines:
-    start = 0
-    end = n-1
-    while start<=end:
-        mid = (start+end)//2
-        if line[0] <= points[mid] <= line[1]:
-            # 중간값의 포인트만이 아닌 나머지의 포인트들도 확인해줘야하는데,
-            # start쪽의 포인트부터 확인인지, end쪽의 포인트부터 확인인지...?
-            # 구현 못 함. 
-            break
-        elif points[mid] < line[0]:
-            end = mid -1
-        elif points[mid] > line[1]:
-            start = mid + 1
-
-print(cnt)
+    start, end = line
+    start_index = binary_search(points, start)
+    end_index = binary_search(points, end + 1)  # end + 1로 설정하여 end가 있는지 확인
+    count = end_index - start_index
+    print(count)
--- week01/test/28701.py
+++ week01/test/28701.py
@@ -4,10 +4,22 @@ sum1 = 0
 sum3 = 0
 for i in range(1, n+1):
     sum += i
+    sum3 += i**3
 
 sum2 = sum**2
 
-for i in range(1,n+1):
-    sum3 += i**3
 
 print(sum, sum2, sum3, sep='\n')
+
+
+# 재귀로 풀기
+# def sum(N):
+#     if N == 1:
+#         return 1
+#     return N + sum(N-1)
+
+# def power(N):
+#     if N == 1:
+#         return 1
+#     return N**3+power(N-1)
+
