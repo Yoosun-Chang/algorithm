3a922ec78744bab8e468c736930b9b4215cf0552
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-05-30 13:57:48 +0900

    Time: 43 ms (98.8%), Space: 49.5 MB (83.85%) - LeetHub

--- LeetCode/0020-valid-parentheses/0020-valid-parentheses.js
+++ LeetCode/0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,14 +3,12 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // 괄호 쌍을 저장하는 맵
     const bracketMap = {
         '(': ')',
         '{': '}',
         '[': ']'
     };
     
-    // 스택 초기화
     const stack = [];
     
     for (let char of s) {
