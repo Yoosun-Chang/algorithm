60be9cb8084ac5d0999f71fb38f35a21a9b29577
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-06-01 20:37:26 +0900

    [Bronze IV] Title: 세수정렬, Time: 104 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -1,13 +1,8 @@
-// fs 모듈을 이용해 파일 전체를 읽어와 문자열로 저장하기
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let arr = input[0].split(' ').map(Number);
-// 오름차순 정렬 수행
-arr.sort(function(a, b) {
-    return a - b;
-})
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+let arr = input.map(Number).sort((a,b)=>a-b);
 let answer = "";
-for (let i = 0; i < arr.length; i++) {
-    answer += arr[i] + " ";
+for (a of arr) {
+    answer += a + " ";
 }
 console.log(answer);
