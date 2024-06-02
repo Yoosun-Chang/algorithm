692b3c2f7b5bf7622e8be84b0c1e550f0d8303c6
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-06-02 10:31:44 +0900

    [Silver V] Title: 단어 정렬, Time: 260 ms, Memory: 19692 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,25 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let n = Number(input[0]);
+let words = [];
+for (let i = 1; i <= n; i++) {
+    words.push(input[i]);
+}
+
+function compare(a, b) {
+    if (a.length === b.length) {
+        return a.localeCompare(b); 
+    } else {
+        return a.length - b.length; 
+    }
+}
+
+words.sort(compare);
+
+let uniqueWords = [...new Set(words)];
+
+let answer = "";
+for (let i = 0; i < uniqueWords.length; i++) {
+    answer += uniqueWords[i] + "\n";
+}
+console.log(answer);
