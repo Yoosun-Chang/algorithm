9cfc81338c2a2c7ffddb9b033502a4684e72069f
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-06-03 22:39:51 +0900

    Time: 59 ms (51.26%), Space: 51.1 MB (74.02%) - LeetHub

---
+++ LeetCode/1436-destination-city/1436-destination-city.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function (paths) {
+    let start = new Set();
+
+    for (path of paths) {
+        start.add(path[0]);
+    }
+
+    for (path of paths) {
+        if (!start.has(path[1])) {
+            return path[1];
+        }
+    }
+}
