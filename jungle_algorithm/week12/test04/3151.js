99481c9318679fe2de2bdda4968c4e51cde5c4b9
Yoosun-Chang <12201321@inha.edu>
2024-06-04 11:23:13 +0900

    week12 test04

---
+++ jungle_algorithm/week12/test04/3151.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) {
+    if(nums.length == 1) return true;
+
+    for(let i = 1; i<nums.length; i++) {
+        if(nums[i]%2 != 0 && nums[i-1]%2 != 0) {
+            return false;
+        }
+        if(nums[i]%2 == 0 && nums[i-1]%2 == 0) {
+            return false;
+        }
+    }   
+    return true;
+
+};
+
+let data = [2,1];
+console.log(isArraySpecial(data))
