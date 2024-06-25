c5ad90b2d4c5e8080fdc5c1540392754d2018a83
Yoosun-Chang <12201321@inha.edu>
2024-06-25 10:00:00 +0900

    0625

--- js_100/2권/~#100/#93_페이지교체_선입선출알고리즘.js
+++ js_100/2권/~#100/#93_페이지교체_선입선출알고리즘.js
@@ -2,7 +2,6 @@ function solution(frame, page){
     let runTime = 0;
     let temp = [];
   
-    // frame이 0일때, page의 크기만큼 6을 곱해주고 끝낸다
     if (frame === 0){
       runTime = page.length * 6;
       return runTime;
@@ -12,16 +11,13 @@ function solution(frame, page){
       if (temp.includes(i)){
         runTime += 1
       } else {
-        // 배열이 비었으면 무조건 넣어야 하므로 if문을 사용
         if (temp.length < frame){
           temp.push(i);
         } else {
-          // 가장 사용되지 않은 첫번째 배열을 제거하고 맨 뒤에 입력값을 저장
           temp.shift();
           temp.push(i);
         }
   
-        // if문 실행에 상관없이 runTime은 10이 추가된다
         runTime += 6;
       }
     }
