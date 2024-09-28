a9e8fab542824f40d905559dcfa6027b240ff367
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-09-28 19:45:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,11 +1,15 @@
 function solution(s) {
-    if (s.length === 4 || s.length === 6) {
-        for (let i = 0; i < s.length; i++) {
-            if (isNaN(parseInt(s[i], 10))) {
-                return false;
-            }
-        }
-        return true;
+    // 길이가 4 또는 6인지 확인
+    if (s.length !== 4 && s.length !== 6) {
+        return false;
     }
-    return false;
+
+    // 모든 문자에 대해 숫자인지 확인
+    for (let i = 0; i < s.length; i++) {
+        if (isNaN(s[i])) {
+            return false;  // 숫자가 아닌 문자가 있을 경우 false 반환
+        }
+    }
+
+    return true;  // 모든 조건을 통과하면 true 반환
 }
