f48e94ecd25221da88f4a78bf593d715adab0c44
유선 <81423123+Yoosun-Chang@users.noreply.github.com>
2024-10-09 19:43:56 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -0,0 +1,23 @@
+function solution(n, computers) {
+    let visited = Array(n).fill(false);
+    let networkCount = 0;
+
+    function dfs(node) {
+        visited[node] = true;
+
+        for (let i = 0; i < n; i++) {
+            if (computers[node][i] === 1 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            networkCount++;
+        }
+    }
+
+    return networkCount;
+}
